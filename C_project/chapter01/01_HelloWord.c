af2701f18d70ee977e3600c98754008186fb7a61
hreo <2020104216@qq.com>
2024-03-15 16:53:47 +0800

    2024/3/15

---
+++ C_project/chapter01/01_HelloWord.c
@@ -0,0 +1,10 @@
+//
+// Created by 郝海程 on 2024/3/15.
+//
+
+//#include <stdio.h>
+//int main(){
+//    printf("hello,c language");
+//    return 0;
+//}
+//注意，一个项目文件夹下只能有一个函数叫main,不然运行会报错
---
+++ C_project/chapter01/03_helloworld的剖析.c
@@ -0,0 +1,16 @@
+//
+// Created by 郝海程 on 2024/3/15.
+//
+#include <stdio.h>
+
+// main()是程序的入口，格式是固定的
+// int :表示函数的返回值类型
+//只要指定了非空的返回值类型，那么就必须要有返回值
+//int main() {
+//    printf("hello world \n");
+//    return 0;
+//}
+
+void main(){
+    printf("hello");
+}
